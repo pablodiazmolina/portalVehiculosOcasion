328828436b18d1d349b2e6b9c7cdca66f282f3a9
Pablo Diaz <pablo.diazmolina@gmail.com>
2019-02-07 15:20:26 +0100

    Subida inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './utilidades/header/header.component';
+import { FooterComponent } from './utilidades/footer/footer.component';
+import { BodyComponent } from './pages/body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
