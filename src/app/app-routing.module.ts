cf841bbe1fbf47b2b6f597a137f3edf5b65f1a0d
Pablo Diaz <pablo.diazmolina@gmail.com>
2019-02-07 17:35:08 +0100

    Añadido enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './pages/body/body.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: BodyComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'itemDetail', component: DetailsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
