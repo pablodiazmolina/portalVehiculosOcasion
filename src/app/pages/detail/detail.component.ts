5482549cc27ea6a3f454d1672164fdf2cfc639bc
Pablo Diaz <pablo.diazmolina@gmail.com>
2019-02-22 13:55:05 +0100

    Recogido parametro en el ts de detalle

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.route.params.subscribe ( parametros => { console.log(parametros ['id']); });
   }
-
 }
