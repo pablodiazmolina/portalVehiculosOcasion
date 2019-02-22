58b1f12305d21a81cb3b9c82bee0a322ee50885e
Pablo Diaz <pablo.diazmolina@gmail.com>
2019-02-22 13:23:32 +0100

    quitado detalle antiguo del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BodyComponent } from './pages/body/body.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { DetailsComponent } from './pages/details/details.component';
 import { DetailComponent } from './pages/detail/detail.component';
 
 const routes: Routes = [
   { path: 'home', component: BodyComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'itemDetail', component: DetailsComponent },
   { path: 'detail', component: DetailComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
