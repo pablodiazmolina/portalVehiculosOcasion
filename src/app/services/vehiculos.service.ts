e6f72e17100586a000998763e21c38b44aab9196
Pablo Diaz <pablo.diazmolina@gmail.com>
2019-02-27 12:40:20 +0100

    listo para producción

--- src/app/services/vehiculos.service.ts
+++ src/app/services/vehiculos.service.ts
@@ -15,19 +15,44 @@ export class VehiculosService {
     this.cargarVehiculos();
   }
 
+  /** Método que carga todos los vehículos */
   private cargarVehiculos() {
-    this.http.get( 'https://angular-citroen-hita.firebaseio.com/vehiculo.json' ).subscribe( (resp: InfoVehiculo []) => {
-      this.vehiculos = resp;
-      this.cargando = false;
+    return new Promise((resolve, reject) => {
+            this.http.get( 'https://angular-citroen-hita.firebaseio.com/vehiculo.json' ).subscribe( (resp: InfoVehiculo []) => {
+        this.vehiculos = resp;
+        this.cargando = false;
+        resolve();
+      });
     });
   }
 
   public buscarVehiculos(cadena: string) {
-    this.vehiculosFiltrados = this.vehiculos.filter ( vehiculo => {
-      this.cargando = false;
-      return true;
-    });
+    if (this.vehiculos.length === 0) {
+      this.cargarVehiculos().then(() => {
+        this.filtrarVehiculos(cadena);
+      });
+    } else {
+      this.filtrarVehiculos(cadena);
+    }
+  }
 
-    console.log(this.vehiculosFiltrados);
+  /**
+   * Método que filtra los vehículos por marca, modelo y precio
+   */
+  private filtrarVehiculos( cadena: string) {
+    this.vehiculosFiltrados = [];
+    cadena = cadena.trim().toLocaleLowerCase();
+
+    this.vehiculosFiltrados = this.vehiculos.filter ( vehiculo => {
+      const marca: string = vehiculo.marca.trim().toLocaleLowerCase();
+      const modelo: string = vehiculo.modelo.trim().toLocaleLowerCase();
+
+      if (marca.search (cadena) >= 0 ||
+          modelo.search (cadena) >= 0 ||
+          cadena.search (vehiculo.precio) >= 0) {
+           return true;
+      }
+    });
+    this.cargando = false;
   }
 }
